b014bac27c3b2cb9797cb0abd44eb0e655e9a350
Francois Smith <steamfaffa@gmail.com>
2022-09-06 09:19:53 +0200

    Finished Test
    Close #12

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 //
 
 #include <iostream>
+#include "TestClass.h"
 
 int main() {
     std::cout << "Lets do this bitches" << std::endl;
     std::cout << "soundcloud.com/keelanriel" << std::endl;
+    std::cout << "Ruck you Ross!" << std::endl;
     return 0;
 }
