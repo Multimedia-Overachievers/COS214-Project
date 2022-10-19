b55fa461d74fd0d31deffffff4158b8f31c0b691
Thomas Isebeck <thomasisebeck@gmail.com>
2022-10-19 22:30:11 +0200

    Added documentation to concreate FactionActions

--- Driver/private/BuildInCountry.cpp
+++ Driver/private/BuildInCountry.cpp
@@ -5,11 +5,10 @@
 #include "../public/BuildInCountry.h"
 
 /**
-* @brief Constructor for the BuildInCountry command
-* @param myFaction The faction to be built in.
+* @brief Constructor for the BuildInCountry command.
+* @param myFaction The faction to be built in (passed to FactionAction).
 * @param myCountry The country to be build in.
 * @param myBuilding The building to be built (pass in an anonymous pointer).
-* @return the ConcreteFaction stored
 */
 BuildInCountry::BuildInCountry(ConcreteFaction * myFaction, Country* myCountry, Building* myBuilding): FactionAction(myFaction) {
     this->myCountry = myCountry;
@@ -17,9 +16,9 @@ BuildInCountry::BuildInCountry(ConcreteFaction * myFaction, Country* myCountry, 
 }
 
 /**
-* @brief Method to get the currently stored faction.
+* @brief passes the building to the faction to be built.
 */
 void BuildInCountry::execute() {
     // Uncommented until functions are made to allow compilation
-    // myFaction->build(this->myBuilding);
+    // myFaction->build(this->myCountry, this->myBuilding);
 }
--- Driver/private/MoveTroops.cpp
+++ Driver/private/MoveTroops.cpp
@@ -4,12 +4,24 @@
 
 #include "../public/MoveTroops.h"
 
+/**
+* @brief Constructor for the MoveTroops command
+* @param destinationFaction The faction to which the troops will be moved.
+* @param destinationCountry The country to which the troops will be moved.
+* @param sourceCountry The country from which to take the troops.
+*/
 MoveTroops::MoveTroops(ConcreteFaction* destinationFaction, Country* destinationCountry, Country* sourceCountry, int numTroops)
                                                                                 : FactionAction(destinationFaction) {
     this->destinationCountry = destinationCountry;
     this->numTroops = numTroops;
 }
 
+/**
+* @brief Constructor for the MoveTroops command
+* @param destinationFaction The faction to which the troops will be moved.
+* @param destinationCountry The country to which the troops will be moved.
+* @param sourceCountry The country from which to take the troops.
+*/
 void MoveTroops::execute() {
     //TODO: remove building buff before you move the troops
     for (int i = 0; this->sourceCountry->hasTroops() && i < this->numTroops; i++)
