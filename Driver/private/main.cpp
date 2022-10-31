74a6eee6a07352e633efa48bea6925faa4bbccfb
Francois Smith <steamfaffa@gmail.com>
2022-10-31 19:39:05 +0200

    Co-authored-by: Ross Tordiffe <rosstordiffe24@gmail.com>

--- Driver/private/main.cpp
+++ Driver/private/main.cpp
@@ -14,11 +14,8 @@ int main()
     // === Main Loop ===
     for (int i = 0; i < MAX_TURNS; i++)
     {
-        // -?Ross- Ask group about casting 
-        
         std::cout << "Turn " << i << " - " << simulator->getFaction(factionTurn)->getName() << " turn" << std::endl;
         Faction* faction = ConcreteSimulator::getInstance()->getFaction(factionTurn);
-        std::cout << "Faction: " << faction->getName() << std::endl;
         ConcreteSimulator::getInstance()->notify(faction);
         
         // Switch turn
