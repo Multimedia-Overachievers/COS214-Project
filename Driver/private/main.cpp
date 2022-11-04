4a72ff43c43537e915e7916aa1dbcc0682b5c475
Francois Smith <steamfaffa@gmail.com>
2022-11-04 18:29:50 +0200

    Added SFML Graphics Library
    Co-authored-by: Ross Tordiffe <rosstordiffe24@gmail.com>

--- Driver/private/main.cpp
+++ Driver/private/main.cpp
@@ -1,38 +1,66 @@
-#include <iostream>
+// #include <iostream>
 
-#include "../public/ConcreteSimulator.h"
+// #include "../public/ConcreteSimulator.h"
 
-int main() 
-{
-    // === Variables ===
-    const int MAX_TURNS = 20;
-    FactionName factionTurn = Allies;
+// int main() 
+// {
+//     // === Variables ===
+//     const int MAX_TURNS = 20;
+//     FactionName factionTurn = Allies;
 
-    // === Create the simulator ===
-    ConcreteSimulator* simulator = ConcreteSimulator::getInstance();
+//     // === Create the simulator ===
+//     ConcreteSimulator* simulator = ConcreteSimulator::getInstance();
 
-    // === Main Loop ===
-    for (int i = 0; i < MAX_TURNS; i++)
-    {
-        Faction* faction = simulator->getFaction(factionTurn);
-        simulator->notify(faction);
-        faction->notify();
+//     // === Main Loop ===
+//     for (int i = 0; i < MAX_TURNS; i++)
+//     {
+//         Faction* faction = simulator->getFaction(factionTurn);
+//         simulator->notify(faction);
+//         faction->notify();
 
-        std::cout << "Turn " << i << " complete." << std::endl;
+//         std::cout << "Turn " << i << " complete." << std::endl;
         
-        // Switch turn
-        if (factionTurn == Allies)
+//         // Switch turn
+//         if (factionTurn == Allies)
+//         {
+//             factionTurn = Axis;
+//         }
+//         else
+//         {
+//             factionTurn = Allies;
+//         }
+//     }
+
+//     return 0;
+// }
+
+// // Misc commits for progress tracking
+// // Started class diagram implementation
+
+
+
+
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Red);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-            factionTurn = Axis;
+            if (event.type == sf::Event::Closed)
+                window.close();
         }
-        else
-        {
-            factionTurn = Allies;
-        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
     }
 
     return 0;
 }
-
-// Misc commits for progress tracking
-// Started class diagram implementation
--- makefile
+++ makefile
@@ -2,22 +2,14 @@ BUILD_DIR := Build
 
 main:
 ifneq ("$(wildcard $(BUILD_DIR))","")
-	rm -d -r $(BUILD_DIR)
-endif
+
+else
 	mkdir $(BUILD_DIR)
+endif
 	cd $(BUILD_DIR); cmake ..; make; ./Project
 
-run: 
-	main
-
 clean:
-	rm main
-	rm -d -r build
-
-reset:
-	make clean
-	make
-	make run
+	rm -rf $(BUILD_DIR)
 
 debug:
 ifneq ("$(wildcard $(BUILD_DIR))","")
@@ -26,10 +18,10 @@ endif
 	mkdir $(BUILD_DIR)
 	cd $(BUILD_DIR); cmake -DCMAKE_BUILD_TYPE=Debug ..; make; gdb ./Project
 	
-
 test:
 ifneq ("$(wildcard $(BUILD_DIR))","")
-	rm -d -r $(BUILD_DIR)
-endif
+
+else
 	mkdir $(BUILD_DIR)
+endif
 	cd $(BUILD_DIR); cmake -D BUILD_TESTS=ON ..; make; ./Testing
