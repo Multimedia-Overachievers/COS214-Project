de805a72379e3b96eb5c582a623f301838159d49
taylawantsasnack <tayla.orsmond@gmail.com>
2022-11-07 00:56:18 +0200

    Simplified restock command

--- Driver/private/Restock.cpp
+++ Driver/private/Restock.cpp
@@ -18,8 +18,7 @@ void Restock::execute() {
     const int NUM_TROOPS = rand() % 20 + 10;
 
     //add the newly created troops
-    for (int i = 0; i < NUM_TROOPS; i++)
-        myCountry->addTroops(new Soldiers(troopName, troopstate));
+    myCountry->addTroops(NUM_TROOPS);
 
     std::cout << "Restocked " << NUM_TROOPS << " troops in " << convert_country[myCountry->getName()] << std::endl;
     std::cout << myCountry->myTroops->getReport() << std::endl;
